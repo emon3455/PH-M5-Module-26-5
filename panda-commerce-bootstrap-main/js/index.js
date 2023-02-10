909e92e60fb2a4954cd381511540a35201736877
emon3455 <emon.mhk69@gmail.com>
2023-02-11 00:30:29 +0600

    3rd task done

--- panda-commerce-bootstrap-main/js/index.js
+++ panda-commerce-bootstrap-main/js/index.js
@@ -4,3 +4,7 @@ let h2= document.getElementsByTagName("h2");
 for (const tag of h2) {
     tag.style.color="lightblue";
 }
+
+// task-3
+document.getElementById("backpack").style.backgroundColor="pink";
+document.getElementById("backpack").style.padding="1rem";
